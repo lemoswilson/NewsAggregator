9d5ceda52370839fc34314fc2ba4975ba5b571a1
lemoswilson <lemoswilson@yahoo.com>
2020-02-05 03:45:09 -0800

    pitchfork and factmag scrappers

--- factmag/scrapper.py
+++ factmag/scrapper.py
@@ -11,6 +11,94 @@ class FactMagScrapper:
         self.news_page = 'https://www.factmag.com/category/news/page/'
         self.tech_page = 'https://www.factmag.com/category/tech/page/'
     
+    class NewsPage:
+        def __init__(self, link, headline=None, author=None, date=None, text=None, img_link=None, content_images=None, content_links = None, iframe = None, tags=None, category=None):
+            self.link = link
+            self.headline = headline
+            self.author = author
+            self.date = date
+            self.text = text
+            self.img_link = img_link
+            self.content_images = content_images
+            self.content_links = content_links
+            self.iframe = iframe
+            self.tags = tags
+            self.category = category
+            self.soup = None
+
+        def __str__(self):
+            return self.headline
+
+        def page_soup(self):
+            if not self.soup:
+                page = requests.get(self.link)
+                content = page.content
+                self.soup = BeautifulSoup(content, "html.parser")
+            return self.soup
+
+        def get_text(self):
+            post_text = self.page_soup().find("div", {"class":"entry-content"})
+            p = post_text.find_all("p")
+            p_content = str()
+            for t in p:
+                p_content += t.text
+            self.text = p_content
+            return self.text
+        
+        def get_tags(self):
+            tags = self.page_soup().find("div", {"class":"article-tags"})
+            self.tags = [tag.text for tag in tags.find_all("a")]
+            return self.tags
+        
+        def get_author(self):
+            self.author = self.page_soup().find("a", {"class":"author"}).text
+            return self.author
+
+        def get_iframe(self):
+            post_text = self.page_soup().find("div", {"class":"entry-content"})
+            if post_text.find("iframe"):
+                iframe = post_text.find_all("iframe")
+                iframe_src = [i['src'] for i in iframe]
+                self.iframe = iframe_src
+            return self.iframe
+
+
+        def get_content_images(self):
+            post_text = self.page_soup().find("div", {"class":"entry-content"})
+            if post_text.find("img"):
+                images = post_text.find_all("img")
+                images_src = []
+                for i in images:
+                    try:
+                        images_src.append([i['src'], i['srcset']])
+                    except:
+                        images_src.append(i['src'])
+                self.content_images = images_src
+            return self.content_images
+        
+        def get_content_links(self):
+            post_text = self.page_soup().find("div", {"class":"entry-content"})
+            if post_text.find("a"):
+                self.content_links = [i['href'] for i in post_text.find_all("a") if i.has_attr("href")]
+            return self.content_links
+
+        def get_date(self):
+            divider = self.page_soup().find("span", {"class":"f-author"})
+            date = divider.nextSibling.nextSibling.nextSibling.text
+            self.date = date
+            return self.date
+
+        def get_all(self):
+            self.get_author()
+            self.get_content_images()
+            self.get_content_links()
+            self.get_date()
+            self.get_iframe()
+            self.get_tags()
+            self.get_text()
+            return self
+
+
     def get_highlight_post(self, category):
         if category == 'tech':
             base_link = self.tech_page
@@ -28,63 +116,10 @@ class FactMagScrapper:
         image = page_soup.find("div", {"class":"spaces-featured-post"})["style"]
         img_link = image.split("url(")[1]
         img_link = img_link[:-1]
-        post_dict = {"link": info['href'], "title": info['title'], "image-link":img_link}
+        return self.NewsPage(info['href'], headline = info['title'], img_link=img_link, category=category)
 
 
-
-
-        post = requests.get(post_dict['link'])
-        post_content = post.content
-        soup_content = BeautifulSoup(post_content, "html.parser")
-
-        divider = soup_content.find("span", {"class":"f-author"})
-        date = divider.nextSibling.nextSibling.nextSibling.text
-        post_dict['date'] = date # date format dd.mm.yy
-
-        post_text = soup_content.find("div", {"class":"entry-content"})
-        if post_text.find("a"):
-            link_list = [i['href'] for i in post_text.find_all("a")]
-            post_dict['content-links'] = link_list
-        else:
-            post_dict['content-links'] = None
-
-        if post_text.find("img"):
-            images = post_text.find_all("img")
-            images_src = []
-            for i in images:
-                try:
-                    images_src.append([i['src'], i['srcset']])
-                except:
-                    images_src.append(i['src'])
-            post_dict['content-images'] = images_src
-        else:
-            post_dict['content-images'] = None
-        
-        p = post_text.find_all("p")
-        p_content = str()
-        for t in p:
-            p_content += t.text
-        
-        tags = soup_content.find("div", {"class":"article-tags"})
-        tag_list = [tag.text for tag in tags.find_all("a")]
-        
-        author = soup_content.find("a", {"class":"author"}).text
-        
-        if post_text.find("iframe"):
-            iframe = post_text.find_all("iframe")
-            iframe_src = [i['src'] for i in iframe]
-            post_dict['iframe'] = iframe_src
-        else:
-            post_dict['iframe'] = None
-        
-        post_dict['tags'] = tag_list
-        post_dict['content'] = p_content
-        post_dict['author'] = author
-        return post_dict
-
-
-    def get_posts_info(self, category, p_number):
-        post_info = {}
+    def get_posts(self, category, p_number):
         if category == 'tech':
             base_link = self.tech_page
         elif category == 'news':
@@ -92,7 +127,7 @@ class FactMagScrapper:
         else:
             return "not valid category, choose between 'news' or 'tech'"
         
-        posts_lists = []
+        posts_list = []
         page = requests.get(base_link + str(p_number))
         page_content = page.content
         page_soup = BeautifulSoup(page_content, "html.parser")
@@ -101,65 +136,12 @@ class FactMagScrapper:
             to_post = div.find("a")
             post_meta = div.find('p', {'class':'meta'})
             img_srcs = [div.find("img")['src'], div.find("img")['srcset']]
-            post_dict = {"link": to_post['href'], "title": to_post['title'], "date":post_meta.find_all("span")[1].text, "image-link":img_srcs}
-            
-            post = requests.get(post_dict['link'])
-            post_content = post.content
-            soup_content = BeautifulSoup(post_content, "html.parser")
+            posts_list.append(self.NewsPage(to_post['href'], headline=to_post['title'], date=post_meta.find_all("span")[1].text, img_link=img_srcs, category=category))
 
-            post_text = soup_content.find("div", {"class":"entry-content"})
-            if post_text.find("a"):
-                link_list = [i['href'] for i in post_text.find_all("a")]
-                post_dict['content-links'] = link_list
-            else:
-                post_dict['content-links'] = None
+        return posts_list
 
-            if post_text.find("img"):
-                images = post_text.find_all("img")
-                images_src = []
-                for i in images:
-                    try:
-                        images_src.append([i['src'], i['srcset']])
-                    except:
-                        images_src.append(i['src'])
-                post_dict['content-images'] = images_src
-            else:
-                post_dict['content-images'] = None
-            
-            p = post_text.find_all("p")
-            p_content = str()
-            for t in p:
-                p_content += t.text
-            
-            tags = soup_content.find("div", {"class":"article-tags"})
-            tag_list = [tag.text for tag in tags.find_all("a")]
-            
-            author = soup_content.find("a", {"class":"author"}).text
-            
-            if post_text.find("iframe"):
-                iframe = post_text.find_all("iframe")
-                iframe_src = [i['src'] for i in iframe]
-                post_dict['iframe'] = iframe_src
-            else:
-                post_dict['iframe'] = None
-            
-            post_dict['tags'] = tag_list
-            post_dict['content'] = p_content
-            post_dict['author'] = author
-            posts_lists.append(post_dict)
-        return posts_lists
 
 scrapper = FactMagScrapper()
-# print(scrapper.get_posts_info('news', 1))
-print(scrapper.get_highlight_post('tech'))
-
-# title
-# link
-# author
-# image-link
-# date
-# text
-# content-images
-# content-links
-# iframe
-# tags
+highlight_news = scrapper.get_highlight_post('news')
+highlight_news.get_all()
+print(highlight_news.author)
--- pitchfork/scrapper.py
+++ pitchfork/scrapper.py
@@ -6,16 +6,161 @@ import json
 
 class Pitchfork:
     def __init__(self):
-        self.base = "https://pitchfork.com/news/?genre=electronic&page="
+        self.news = "https://pitchfork.com/news/?genre=electronic&page="
+        self.base = "https://pitchfork.com"
 
-    def get_news(page_number):
-        url = self.base + page_number
+    def get_news(self, page_number):
+        news_list = []
+        url = self.news + str(page_number)
+        page = requests.get(url)
+        content = page.content
+        sopa = BeautifulSoup(content, "html.parser")
+        news = sopa.find(id="news-page")
+
+        latest_hero = news.find("div", {"class":"latest-hero"}) 
+        hero_img = latest_hero.find("img")['src']
+        hero_link = self.base + latest_hero.find('a')['href']
+        hero_headline = latest_hero.find("h2", {"class":"title"}).text
+        hero_description = latest_hero.find("div", {"class":"abstract"}).text
+        meta = latest_hero.find("div", {"class":"meta"})
+        hero_date = meta.find("time")["title"]
+        news_list.append(self.NewsPage(hero_link, img_link = hero_img, headline = hero_headline, description=hero_description, date=hero_date)) 
+
+        second_tier = news.find("section", {"class":"second-tier"})
+        second_tier_news = second_tier.find_all("div", {"class":"news-module"})
+        for n in second_tier_news:
+            news_img = n.find("img")['src']
+            news_link = self.base + n.find("a")['href']
+            news_headline = n.find("h2", {"class":"title"}).text
+            news_date = n.find("time")['title']
+            news_list.append(self.NewsPage(news_link, img_link = news_img, headline = news_headline, date = news_date))
+
+        third_tier = news.find("section", {"class":"third-tier"})
+        third_tier_news = third_tier.find_all("div", {"class":"news-module"})
+        for n in third_tier_news:
+            news_img = n.find("img")['src']
+            news_link = self.base + n.find("a")['href']
+            news_headline = n.find("h2", {"class":"title"}).text
+            news_date = n.find("time")['title']
+            news_list.append(self.NewsPage(news_link, img_link = news_img, headline = news_headline, date = news_date))
+
+        return news_list
 
 
     class NewsPage:
-        def __init__(self, link, )
+        def __init__(self, link, headline=None, description=None, author=None, date=None, text=None, img_link=None, content_images=None, content_links = None, iframe = None, tags=None, artists=None):
+            self.link = link
+            self.headline = headline
+            self.description = description
+            self.author = author
+            self.img_link = img_link
+            self.date = date
+            self.text = text
+            self.content_links = content_links
+            self.content_images = content_images
+            self.iframe = iframe
+            self.tags = tags
+            self.artists = artists
+            self.soup = None    
 
-# divided in three sections 
-# 1 - <section class = "hero"
-# 2 - <div class = "container-fluid"
-# 3 - <div class= "lower-section"
+        def __str__(self):
+            return self.headline
+        
+        def page_soup(self):
+            if not self.soup:
+                page = requests.get(self.link)
+                content = page.content
+                soup = BeautifulSoup(content, "html.parser")
+                self.soup = soup
+            return self.soup
+
+        def article(self):
+            return self.page_soup().find("article").find("div", {"class":"article-content"})
+
+        def get_headline(self):
+            if not self.headline:
+                self.headline = self.article().find("h1").text
+            return self.headline
+        
+        def get_description(self):
+            if not self.description:
+                self.description = self.article().find("h2").text
+            return self.description
+
+        def get_date(self):
+            if not self.date:
+                self.date = self.article().find("time")["title"] # time format "weekday Mon dd yyyy hh:mm:ss FUSO"
+            return self.date
+        
+        def get_author(self):
+            try:
+                ul = self.page_soup().find("ul", {"class":"authors-detail"})
+                authors = ul.find_all("li")
+                self.author = [i.find("a").text for i in authors]
+            except:
+                div = self.page_soup().find("div", {"class":"authors-detail"})
+                authors = div.find_all("a")
+                self.author = [i.text for i in authors]
+            return self.author
+        
+        def get_img_link(self):
+            if not self.img_link:
+                self.img_link = self.article().find("img")['src']
+            return self.img_link
+
+        def get_text(self):
+            contents = self.article().find("div", {"class":"contents"})
+            elements = contents.find_all()
+            self.text = "".join([element.text for element in elements if element.name != "figure"])
+
+        def get_content_links(self):
+            contents = self.article().find("div", {"class":"contents"})
+            if contents.find("a"):
+                self.content_links = [i['href'] for i in contents.find_all("a") if i.has_attr("href")]
+            return self.content_links
+        
+        def get_content_images(self):
+            contents = self.article().find("div", {"class":"contents"})
+            if contents.find("img"):
+                self.content_images = [i['src'] for i in contents.find_all("img")if i.has_attr("src")]
+            return self.content_images
+
+        def get_iframe(self):
+            contents = self.article().find("div", {"class":"contents"})
+            if contents.find("iframe"):
+                self.iframe = [i['src'] for i in contents.find_all("iframe") if i.has_attr("src")]
+            return self.iframe
+        
+        def get_tags(self):
+            aside = self.page_soup().find("aside")
+            tag_section = aside.find_all("section")[1]
+            tag_list = tag_section.find_all("li")
+            self.tags = [i.text for i in tag_list]
+            return self.tags
+
+        def get_artists(self):
+            aside = self.page_soup().find("aside")
+            artists_section = aside.find_all("section")[0]
+            artists_list = artists_section.find_all("li")
+            self.artists = [i.text for i in artists_list]
+
+        def get_all(self):
+            self.get_artists()
+            self.get_author()
+            self.get_content_images()
+            self.get_content_links()
+            self.get_date()
+            self.get_description()
+            self.get_headline()
+            self.get_iframe()
+            self.get_img_link()
+            self.get_tags()
+            self.get_text()
+            return self
+
+
+scrapper = Pitchfork()
+news_p1 = scrapper.get_news(1)
+news2 = news_p1[2]
+news2.get_all()
+print(news2.tags)
